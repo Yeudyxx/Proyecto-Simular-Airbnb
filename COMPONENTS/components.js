d99fafcc266c61c4b7d20bb7375924999bc156c5
Yeudy Rosales <167600042+Yeudyxx@users.noreply.github.com>
2025-05-24 18:09:38 -0600

    Add files via upload

---
+++ COMPONENTS/components.js
@@ -0,0 +1,29 @@
+const Header=()=>{
+    return  `<div><a href="product.html"><img id="navbarlogo" src="../img/long-logo.png" alt=""></a></div>
+    <div class="searchbar">
+    <div id="searchbartext">Anywhere</div>
+    <div id="searchbartext">Any Week</div>
+    <div id="searchbartext2">Add guests</div>
+    <div class="search-icon-div">
+    <span id="search-icon"><i class="fas fa-search"></i>
+    </div>
+    </div>
+    <div class="profile-container">
+    <div class="airbnb-your">Airbnb your home</div>
+    <div class="airbnb-your"><i class="fa-solid fa-globe"></i></div>
+    <div class="profileIcon">
+    <div id="profiletoggle"><i class="fa fa-bars"></i><span><i class="fa fa-user"></i></span></div>
+    <div id="profilemodal">
+    <div><a id="sign-button" class="button">Sign up</a></div>
+    <div><a href="" id="login_btn">Log in</a></div>
+    </hr>
+    <div><a href="wishList.html" id="wishlist_btn">Airbnb your home</a></div>
+    <div><a href="">Host an Experience</a></div>
+    <div><a href="">Help</a></div>
+    </div>
+  </div>
+  </div>
+</div>`; 
+}
+  export{Header};
+  
---
+++ COMPONENTS/payment-process C/payment-Header.js
@@ -0,0 +1,10 @@
+const paymentHeader = function () {
+    return `<div>
+        <a href="/Project_Airbnb.com/HTML/product.html">   <img src="https://imgpile.com/images/dwDX2S.png" alt=""></a>     
+            </div>
+`
+}
+
+export default paymentHeader;
+
+
